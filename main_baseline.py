5f817af49cb3abc217fca8c32f250afd2670e925
TheFreeMan21 <bogdanvanghelie67@gmail.com>
2025-05-25 12:42:36 +0200

    Fixed some issues with splitting the data(Matej)

--- main_baseline.py
+++ main_baseline.py
@@ -56,22 +56,20 @@ for imgAndMask in imageBatch:
     imgIDx += 1
 
 ## Training and testing data NOTE: random state to keep the test always the same
-from sklearn.model_selection import StratifiedKFold
-skf=StratifiedKFold(5,shuffle=True,random_state=42)
+from sklearn.model_selection import StratifiedShuffleSplit
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 
 ## Split into features and cancer values
 x=df.drop(columns='Cancer')
 y=df['Cancer']
 
 ## Split based on cancer values
-for train_idx, test_idx in skf.split(x, y):
+for train_idx, test_idx in sss.split(x, y):
     train_df = df.iloc[train_idx]  # 80%
     test_df = df.iloc[test_idx]    # 20%
-    break
 ## Gives training data (80%) and testing data (20%)
 
 ## Training and validation
-from sklearn.model_selection import StratifiedShuffleSplit
 sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
 
 ## Split into features and cancer values
@@ -80,14 +78,14 @@ y = train_df['Cancer']
 
 ## Split based on cancer values
 for train_idx, test_idx in sss.split(x, y):
-    train_df = df.iloc[train_idx]  # 80%
-    valid_df = df.iloc[test_idx]    # 20%
+    final_train_df = train_df.iloc[train_idx]  # 80%
+    valid_df = train_df.iloc[test_idx]    # 20%
 ## Takes previous training data
 ## Gives training data (80%) and validation data (20%)
 
 ## Remove 'Cancer' for normalizing features
-train_df_cancer=train_df.copy()
-train_df=train_df.drop(columns='Cancer')
+train_df_cancer=final_train_df.copy()
+train_df=final_train_df.drop(columns='Cancer')
 
 valid_df_cancer=valid_df.copy()
 valid_df=valid_df.drop(columns='Cancer')
@@ -109,4 +107,3 @@ test_scaled=scaler.transform(test_df)
 test_df_scaled=pd.DataFrame(test_scaled,columns=columns,dtype=np.float64)
 
 classifier(train_df_scaled, train_df_cancer, valid_df_cancer, valid_df_scaled,test_df_scaled,test_df_cancer)
-
--- main_extended.py
+++ main_extended.py
@@ -77,22 +77,20 @@ def zero_two(x):
 df_new['transformed']= df_new['model'].apply(zero_two)
 
 ## Training and testing data NOTE: random state to keep the test always the same
-from sklearn.model_selection import StratifiedKFold
-skf=StratifiedKFold(5,shuffle=True,random_state=42)
+from sklearn.model_selection import StratifiedShuffleSplit
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 
 ## Split into features and cancer values
 x=df.drop(columns='Cancer')
 y=df['Cancer']
 
 ## Split based on cancer values
-for train_idx, test_idx in skf.split(x, y):
+for train_idx, test_idx in sss.split(x, y):
     train_df = df.iloc[train_idx]  # 80%
     test_df = df.iloc[test_idx]    # 20%
-    break
 ## Gives training data (80%) and testing data (20%)
 
 ## Training and validation
-from sklearn.model_selection import StratifiedShuffleSplit
 sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
 
 ## Split into features and cancer values
@@ -101,14 +99,14 @@ y = train_df['Cancer']
 
 ## Split based on cancer values
 for train_idx, test_idx in sss.split(x, y):
-    train_df = df.iloc[train_idx]  # 80%
-    valid_df = df.iloc[test_idx]    # 20%
+    final_train_df = train_df.iloc[train_idx]  # 80%
+    valid_df = train_df.iloc[test_idx]    # 20%
 ## Takes previous training data
 ## Gives training data (80%) and validation data (20%)
 
 ## Remove 'Cancer' for normalizing features
-train_df_cancer=train_df.copy()
-train_df=train_df.drop(columns='Cancer')
+train_df_cancer=final_train_df.copy()
+train_df=final_train_df.drop(columns='Cancer')
 
 valid_df_cancer=valid_df.copy()
 valid_df=valid_df.drop(columns='Cancer')
@@ -126,5 +124,9 @@ columns = valid_df.columns
 train_df_scaled = pd.DataFrame(train_scaled,columns=columns,dtype=np.float64)
 valid_df_scaled= pd.DataFrame(valid_scaled,columns=columns,dtype=np.float64)
 
-classifier(train_df_scaled, train_df_cancer, valid_df_cancer, valid_df_scaled)
+test_df_cancer=test_df.copy()
+test_df=test_df.drop(columns='Cancer')
+test_scaled=scaler.transform(test_df)
+test_df_scaled=pd.DataFrame(test_scaled,columns=columns,dtype=np.float64)
 
+classifier(train_df_scaled, train_df_cancer, valid_df_cancer, valid_df_scaled,test_df_scaled,test_df_cancer)
