ffbab5fedef3ba2a1c934dbc58b7eced5def39b3
TheFreeMan21 <bogdanvanghelie67@gmail.com>
2025-05-30 13:38:00 +0200

    Deleted the last files which are not required

--- important files to be deleted/baseline model - ABC features/mask_function.py
+++
@@ -1,8 +0,0 @@
-from skimage.color import rgb2gray
-from skimage.filters import gaussian, threshold_otsu
-def get_mask(image):
-    gray_im = rgb2gray(image)
-    blurred_im = gaussian(gray_im, sigma=1.0)
-    t=threshold_otsu(blurred_im)
-    mask = gray_im<t
-    return mask
